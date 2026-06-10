9ecb27e262a5ba859e84ad79550e1b1adfa0e90a
Prince4Iyke <151050678+Prince4Iyke@users.noreply.github.com>
2026-06-10 16:27:03 +0100

    Add files via upload

---
+++ WEEK 14 ASSIGNMENT/Components/navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+    return `
+        <nav class="navbar">
+            <h2>AI Helper</h2>
+
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    `;
+}
