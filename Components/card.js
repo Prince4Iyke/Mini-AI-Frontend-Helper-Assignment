e5727479c8e74d85d8d8032a43a7f722ab411697
Prince4Iyke <151050678+Prince4Iyke@users.noreply.github.com>
2026-06-10 16:18:29 +0100

    Add files via upload

---
+++ Components/card.js
@@ -0,0 +1,13 @@
+function Card(title, description) {
+    return `
+        <div class="card">
+
+            <h3>${title}</h3>
+
+            <p>${description}</p>
+
+            <button>Read More</button>
+
+        </div>
+    `;
+}
